a7211c8396190b884c2b75a12b348e29ef3945bb
iamsatyanchal <quickhalewebtech@gmail.com>
2026-06-27 19:14:51 +0530

    satya//7YA ~ Let's do it :)

--- main.zig
+++ main.zig
@@ -6,12 +6,5 @@ const fmtallocprint = std.fmt.allocPrint;
 const http = std.http;
 
 pub fn main() !void {
-    var gpa = heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    const url = try fmtallocprint(allocator, "http://api-point-ip-details.vercel.app/?ip={}", .{"192.168.1.1"});
-    defer allocator.free(url);
-
-    // after writing all these codes.. i came to know that this will not work with web assembly (wasm).. what a bad day.. :/
+    show("satya//7YA", .{});
 }
